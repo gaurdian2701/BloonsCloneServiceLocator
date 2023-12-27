374d27db500c707456838a7d75f5e2e8dee753f3
kiran2701 <gaurdian.kiran@gmail.com>
2023-12-27 13:14:26 +0530

    Finshed Service Locator by adding UIService and Sound Service to Game Service

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -2,17 +2,31 @@ using ServiceLocator.Utilities;
 using System.Collections;
 using System.Collections.Generic;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
 using UnityEngine;
-
+using ServiceLocator.UI;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 public class GameService : GenericMonoSingleton<GameService>
 {
-    public PlayerScriptableObject playerScriptableObject;
+    [Header("PLAYER")]
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+
+    [Header("SOUND")]
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
     public PlayerService playerService { get; private set; }
+    public SoundService soundService { get; private set; }
+
+    [Header("UI")]
+    [SerializeField] private UIService uiService;
+    public UIService UIService => uiService;
 
     private void Start()
     {
         playerService = new PlayerService(playerScriptableObject);
+        soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
     }
 
     private void Update()
